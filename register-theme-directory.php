1ede1d972ed780a2efb0caf2cab79b7124b299d8
Kenan SALTIK <ksaltik@hotmail.com>
2020-06-22 17:54:18 +0300

    Register New Theme Directory mu-plugin
    First commit

---
+++ register-theme-directory.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:  Register Theme Directory
+ * Plugin URI:   https://github.com/
+ * Description:  Register new theme directory
+ * Version:      1.0.0
+ * Author:       Kenan SALTIK
+ * Author URI:   https://oneyhosting.com/
+ * License:      MIT License
+ */
+
+register_theme_directory( WP_CONTENT_DIR . '/skins' );
